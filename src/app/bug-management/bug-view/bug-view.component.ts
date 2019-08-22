9d7db632bfd971a904a4d4e603a594779807e52f
Klaus Siegel <klaussiegel2@gmail.com>
2019-08-22 13:50:17 +0300

    Routing.

---
+++ src/app/bug-management/bug-view/bug-view.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-bug-view',
+  templateUrl: './bug-view.component.html',
+  styleUrls: ['./bug-view.component.scss']
+})
+export class BugViewComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
