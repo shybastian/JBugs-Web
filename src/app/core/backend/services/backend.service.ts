c1f1292251c1aa0f9043a64eb3c3c5aac4b5ba85
Miruna Dinu <mirunaralucadinu@ymail.com>
2019-08-27 23:01:47 +0300

    ModifyBugStatus - Fixed

--- src/app/core/backend/services/backend.service.ts
+++ src/app/core/backend/services/backend.service.ts
@@ -68,7 +68,7 @@ export class BackendService {
       params,
       headers: {
         'Accept-Language': 'en',
-        'Content-Type': 'application/json'
+        'Content-Type': '*/*'
       }
     };
     if (!url) {
