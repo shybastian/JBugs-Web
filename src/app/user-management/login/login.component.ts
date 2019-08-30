01ac34446ecdc48c72c2ef2f881f770a64f8f756
dianagabrielapopescu <diana.popescu000@gmail.com>
2019-08-30 08:59:19 +0300

    bugs export with i18n (#19)
    * bugs export with i18n
    
    * detailed bug exported to pdf
    
    * i18n for export to PDF
    fixed captcha to generate new one at logout

--- src/app/user-management/login/login.component.ts
+++ src/app/user-management/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Router} from '@angular/router';
 import {CaptchaService} from './services/captcha.service';
@@ -12,7 +12,7 @@ import {StorageService} from './services/storage.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public static SUCCESS_RESPONSE_MESSAGE = 'SUCCESS';
 
@@ -21,6 +21,10 @@ export class LoginComponent {
   constructor(public router: Router, private loginService: LoginService,
               private captchaService: CaptchaService, private cryptoService: CryptoService,
               public storageService: StorageService, public translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.captchaService.firedOnce = false;
   }
 
   login(loginForm: NgForm) {
